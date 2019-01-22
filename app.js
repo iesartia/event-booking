51627494b1d3e4b46838979902570e58b27b21eb
Ilia Esartia <iesartia@gmail.com>
2019-01-22 18:03:48 +0400

    Mongoose save data

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const graphqlHttp = require('express-graphql');
 const { buildSchema } = require('graphql');
 const mongoose = require('mongoose');
 
-const app = express();
+const Event = require('./models/event');
 
-const events = [];
+const app = express();
 
 app.use(bodyParser.json());
 
@@ -42,19 +42,36 @@ app.use('/graphql', graphqlHttp({
     `),
     rootValue: {
         events: () => {
-            return events;
+            return Event
+            .find()
+            .then( events => {
+                return events.map( event => {
+                    return { ...event._doc }
+                });
+            })
+            .catch( err => {
+                throw err;
+            });
         },
         createEvent: (args) => {
-            const event = {
-                _id: Math.random().toString(),
+            
+            const event = new Event({
                 title: args.eventInput.title,
                 description: args.eventInput.description,
                 price: +args.eventInput.price,
-                date: args.eventInput.date
-            };
+                date: new Date(args.eventInput.date)
+            });
 
-            events.push(event);
-            return event;            
+            return event
+            .save()
+            .then(result => {
+                console.log(result);
+                return { ...result._doc };
+            })
+            .catch(err => {
+                console.log(err);
+                throw err;
+            });       
 
         }
     },
@@ -63,7 +80,7 @@ app.use('/graphql', graphqlHttp({
 
 mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${
     process.env.MONGO_PASSWORD
-}@cluster-uvjbc.mongodb.net/test?retryWrites=true
+}@cluster-uvjbc.mongodb.net/${process.env.MONGO_DB}?retryWrites=true
 `).then(() => {
     app.listen(3000);
 }).catch(err => {
