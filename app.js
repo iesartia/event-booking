9e9ec1f4d36304662018ec5a81615e496ed5d8cb
Ilia Esartia <iesartia@gmail.com>
2019-01-23 18:57:51 +0400

    Add dynamic relations

--- app.js
+++ app.js
@@ -1,129 +1,24 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const graphqlHttp = require('express-graphql');
-const { buildSchema } = require('graphql');
 const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
-
-const Event = require('./models/event');
-const User = require('./models/user');
+const graphQlSchema = require('./graphql/schema/index');
+const graphQlResolvers = require('./graphql/resolvers/index');
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.use('/graphql', graphqlHttp({
-    schema: buildSchema(`
-        type Event {
-            _id: ID!
-            title: String!
-            description: String!
-            price: Float!
-            date: String!
-        }
 
-        type User {
-            _id: ID!
-            email: String!
-            password: String
-        }
 
-        input EventInput {
-            title: String!
-            description: String!
-            price: Float!
-            date: String!
-        }
-
-        input UserInput {
-            email: String!
-            password: String!
-        }
-    
-        type RootQuery {
-            events: [Event!]!
-        }
-
-        type RootMutation {
-            createEvent(eventInput: EventInput): Event
-            createUser(userInput: UserInput): User
-        }
-    
-        schema {
-            query: RootQuery
-            mutation: RootMutation
-        }
-    `),
-    rootValue: {
-        events: () => {
-            return Event
-            .find()
-            .then( events => {
-                return events.map( event => {
-                    return { ...event._doc }
-                });
-            })
-            .catch( err => {
-                throw err;
-            });
-        },
-        createEvent: (args) => {
-            
-            const event = new Event({
-                title: args.eventInput.title,
-                description: args.eventInput.description,
-                price: +args.eventInput.price,
-                date: new Date(args.eventInput.date),
-                creator: '5c481eb62b9e184f25d41c0c'
-            });
-
-            let createdEvent;
-
-            return event
-            .save()
-            .then(result => {
-                createdEvent = { ...result._doc };
-                return User.findById('5c481eb62b9e184f25d41c0c');                
-            })
-            .then(user => {
-                if(!user){
-                    throw new Error("User does not exists");
-                }
-                user.createdEvents.push(event);
-                return user.save();
-            })
-            .then(result => {
-                return createdEvent;
-            })
-            .catch(err => {
-                console.log(err);
-                throw err;
-            });
-        },
-        createUser: args => {
-            return User.findOne({ email: args.userInput.email }).then( user => {
-                if(user){
-                    throw new Error("User with same email allready exists");
-                }
-                return bcrypt.hash(args.userInput.password, 12)
-            })            
-            .then(hashedPassword => {
-                const user = new User({
-                    email: args.userInput.email,
-                    password: hashedPassword
-                });
-                return user.save();
-            })
-            .then(result => {
-                return {...result._doc, password: null };
-            })
-            .catch(err => {
-                throw err;
-            });            
-        }
-    },
-    graphiql: true
-}))
+app.use(
+    '/graphql', 
+    graphqlHttp({
+        schema: graphQlSchema,
+        rootValue: graphQlResolvers,
+        graphiql: true
+    })
+);
 
 mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${
     process.env.MONGO_PASSWORD
